00ddc557e2291ef91a5f68f04f5969757c4754df
JLD43 <uc2023214314@student.uc.pt>
2026-03-09 01:31:40 +0000

    done

--- comment_test.java
+++ comment_test.java
@@ -1,24 +1,3 @@
-// 1. Comentário de linha simples
-// Este comentário tem /* e */ lá dentro, mas não deve ativar o estado COMMENT
-
-/* 2. Comentário de bloco simples */
-
-/* 3. Comentário de bloco
-   que ocupa várias
-   linhas e termina aqui -> */
-
-// 4. Teste de operadores colados a comentários
-int x = 5; //*****/
-int y = 10; /*---*/
-
-/**/ /* 5. Bloco vazio e bloco logo a seguir */
-
-// 6. O caso "traiçoeiro" que vimos antes
-//************/ 
-/*
-  Conteúdo aleatório
-**/ 
-
-/* 7. O GRANDE FINAL: Comentário não terminado
-   Este comentário começa aqui e nunca fecha.
-   O lexer deve reportar o erro na linha onde este /* começou.
+public static void main(String[] args) {
+  String str = "\n \r \n";
+}
